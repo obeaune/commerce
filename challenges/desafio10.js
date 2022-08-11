94cc91d0eff1589ede3159e0132ddbd2e5428d73
obeaune <linecora86@gmail.com>
2022-08-11 17:16:02 -0300

    feat: return the `name` of all snacks that have a protein percentage greater than or equal to 30 and less than or equal to 40

---
+++ challenges/desafio10.js
@@ -0,0 +1,9 @@
+// db.produtos.find(
+//   { valoresNutricionais: { $elemMatch : { tipo: "proteínas",
+//   $and: [{ percentual: { $gte: 30 } }, { percentual: { $lte: 40 }}] } }},
+//   { _id: 0, nome: 1 } );
+
+db.produtos.find(
+  { valoresNutricionais: { $elemMatch : { tipo: "proteínas",
+    percentual: { $gte: 30, $lte: 40 } }}},
+  { _id: 0, nome: 1 } );
